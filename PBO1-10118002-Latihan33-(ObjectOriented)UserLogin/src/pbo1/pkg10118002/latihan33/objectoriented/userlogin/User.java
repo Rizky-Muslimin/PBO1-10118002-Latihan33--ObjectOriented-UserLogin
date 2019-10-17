6d00e09138070ed6fcacaaa5221e50159c431bba
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-17 20:05:39 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan33-(ObjectOriented)UserLogin/src/pbo1/pkg10118002/latihan33/objectoriented/userlogin/User.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan33.objectoriented.userlogin;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rizky Muslimin
+ * KELAS    : IF-1
+ * NIM      : 10118002
+ * Deskripsi Program : Program ini untuk membuat data akun.
+ */
+public class User {
+    private static String username;
+    private static String password;
+    private boolean statusAkun;
+
+    private boolean cekAkun(String parUserName, String parPassword){
+        username = "Riski12";
+        password = "Rizkymuslimin";
+        if (parPassword.equals(password) && parUserName.equals(username)) {
+            statusAkun = true;
+        } else {
+            statusAkun = false;
+        }
+        return statusAkun;
+    }
+
+    private void hasilLogin(boolean status, String parUserName){
+        if (status==true) {
+            System.out.println("\n******HALLO "+parUserName.toUpperCase()+"******");
+            System.out.println("Selamat Datang di Aplikasi ini");
+        }else {
+            System.out.println("\nOoops, Username atau Password anda salah");
+        }
+    }
+
+    public void pengecekkanLogin(String parUserName, String parPassword){
+        cekAkun(parUserName,parPassword);
+        hasilLogin(statusAkun,parUserName);
+    }
+    
+}
